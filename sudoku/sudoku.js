9ce2276a9afa13f383ab6ecaf3b3f467d129d1b2
zhongjianhua163@github.com <zhongjianhua163@github.com>
2024-04-05 13:41:26 +0800

    修改清除笔记时的问题

--- sudoku/sudoku.js
+++ sudoku/sudoku.js
@@ -576,17 +576,20 @@ var sudoku = {
         rec.forEach((item) => {
             this.cells.forEach((row, r) => {
                 row.forEach((cell, c) => {
-                    if(item.value == 0){
-                        cell.clearPencil();
-                        return;
-                    }
                     if (!cell.isPencil(item.value)) {
                         return;
                     }
                     if (item.row == cell.row   // same row
                         || item.col == cell.col // same column
-                        || (parseInt(item.row / 3) == parseInt(cell.row / 3) && parseInt(item.col / 3) == parseInt(cell.col / 3))) {
-                        cell.delPencil(item.value);
+                        || (parseInt(item.row / 3) == parseInt(cell.row / 3) // same box
+                            && parseInt(item.col / 3) == parseInt(cell.col / 3))
+                    ) {
+                        if (item.value == 0) {
+                            cell.clearPencil();
+                        }
+                        else {
+                            cell.delPencil(item.value);
+                        }
                     }
                 })
             });
@@ -752,21 +755,8 @@ var sudoku = {
     },
 };
 
+sudoku.initByStr('@3!78#4@1=62&95@9!146#21%4!9(235!8$6@2!7@');
 
-// Example usage
-const sudokuBoard = [
-    [5, 3, 0, 0, 7, 0, 0, 0, 0],
-    [6, 0, 0, 1, 9, 5, 0, 0, 0],
-    [0, 9, 8, 0, 0, 0, 0, 6, 0],
-    [8, 0, 0, 0, 6, 0, 0, 0, 3],
-    [4, 0, 0, 8, 0, 3, 0, 0, 1],
-    [7, 0, 0, 0, 2, 0, 0, 0, 6],
-    [0, 6, 0, 0, 0, 0, 2, 8, 0],
-    [0, 0, 0, 4, 1, 9, 0, 0, 5],
-    [0, 0, 0, 0, 8, 0, 0, 7, 9]
-];
-
-sudoku.init(sudokuBoard);
 //sudoku.solve();
 var sm = document.getElementById('selNum').getElementsByClassName('game-cell');
 for (var i = 0; i < sm.length; i++) {
